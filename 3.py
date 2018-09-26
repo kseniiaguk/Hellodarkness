9b9f0a25bcca17260a9c4b253fe450a377ea86a7
kseniiaguk <43299642+kseniiaguk@users.noreply.github.com>
2018-09-26 21:21:57 +0300

    smth new2

---
+++ 3.py
@@ -0,0 +1,14 @@
+def rotate(x):
+    y=[0]*len(x)
+    for i in range(0,len(x)):
+        if i!=0:
+            y[i]=x[i-1]
+        else:
+            y[i]=x[len(x)-1]
+    return (y)
+n=int(input())
+a=[]
+for i in range (0,n):
+    a.append(int(input()))
+c=rotate(a)
+print(c)
