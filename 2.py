fc6e664250279bfb963736505d768d36a9370ae8
kseniiaguk <43299642+kseniiaguk@users.noreply.github.com>
2018-09-26 17:58:50 +0300

    Add files via upload

--- 2.py
+++ 2.py
@@ -7,12 +7,23 @@ def calc(x,y,z):
         if y=='*':
             return (int(x)*int(z))
         if y=='/':
-            if z!=0:
+            try:
                 return (int(x)/int(z))
-            else:
-                return ('Нельзя делить на 0')
+            except ZeroDivisionError:
+                print ('Нельзя делить на 0')
     else:
         return ('Неизвестная операция')
-a,b,c=map(str,input().split())
-x=calc(a,b,c)
+a=str(input())
+x1=str('')
+y1=str('')
+z1=str('')
+for i in range(0,len(a)):
+   if a[i].isdigit()==False:
+       x1+=a[0:i]
+       y1+=a[i]
+       z1+=a[i+1:len(a)]
+x2=int(x1)
+y2=y1
+z2=int(z1)
+x=calc(x2,y2,z2)
 print(x)
