5f0b8e5e96b0c3fb0013e01e7b35a17f7488cdfe
kseniiaguk <43299642+kseniiaguk@users.noreply.github.com>
2019-04-17 00:25:15 +0300

    update
    smth new

--- sem2/src/ru/spbu/apmath/prog/hw1/rotate3/Main.java
+++ sem2/src/ru/spbu/apmath/prog/hw1/rotate3/Main.java
@@ -1,16 +1,33 @@
-package ru.spbu.apmath.prog.hw1.rotate3;
-
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String myStr = scan.nextLine();
-        ArrayList myList = new ArrayList();
-        for (int i = 0; i < myStr.length(); i++) {
-            myList.add(myStr.charAt(i));
+        System.out.println("Введите количество элементов");
+        Scanner scan1 = new Scanner(System.in);
+        ArrayList<Integer> myList = new ArrayList<>();
+        try {
+            int number = scan1.nextInt();
+            if (number == 0) {
+                Rotate myList1 = new Rotate();
+                System.out.println(myList1.rotate(myList));
+            } else {
+                try {
+                    for (int i = 0; i < number; i++) {
+                        System.out.println("Введите элемент массива");
+                        Scanner scan = new Scanner(System.in);
+                        int myInt = scan.nextInt();
+                        myList.add(myInt);
+                    }
+                    Rotate myList1 = new Rotate();
+                    System.out.println(myList1.rotate(myList));
+                } catch (IndexOutOfBoundsException e2) {
+                    System.out.println("Неверный ввод");
+                }
+            }
         }
-        Rotate myList1 = new Rotate();
-        System.out.println(myList1.rotate(myList));
+        catch (InputMismatchException e){
+            System.out.println("Неверный ввод");
+        }
     }
 }
