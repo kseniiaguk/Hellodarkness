4312cfd8d626124933b79250e0c01329db3e0f16
kseniiaguk <43299642+kseniiaguk@users.noreply.github.com>
2019-04-17 00:21:42 +0300

    update
    smth new

--- sem2/src/ru/spbu/apmath/prog/hw1/bin1/Binar.java
+++ sem2/src/ru/spbu/apmath/prog/hw1/bin1/Binar.java
@@ -7,12 +7,17 @@ public class Binar {
         this.num = num;
     }
     public String ans = "";
-    public String bin(){
+    public String toBin(){
         int deg = 0;
-        if (num > 0) {
-            int div = num;
-            while (num >= 1) {
-                num /= 2;
+        int myNum = num;
+        if (myNum != 0) {
+            if (myNum < 0){
+                ans += "-";
+                myNum = -myNum;
+            }
+            int div = myNum;
+            while (myNum >= 1) {
+                myNum /= 2;
                 deg++;
             }
             for (int i = deg - 1; i >= 0; i--) {
@@ -24,29 +29,10 @@ public class Binar {
                 }
                 deg--;
             }
-        }
-        else if (num == 0){
-            ans = "0";
         }
         else {
-            int num1 = -num;
-            int div = num1;
-            while (num1 >= 1) {
-                num1 /= 2;
-                deg++;
-            }
-            ans += "-";
-            for (int i = deg - 1; i >= 0; i--) {
-                if (div >= Math.pow(2, i)) {
-                    ans += "1";
-                    div -= Math.pow(2, i);
-                } else {
-                    ans += "0";
-                }
-                deg--;
-            }
+            ans = "0";
         }
         return ans;
     }
 }
-
