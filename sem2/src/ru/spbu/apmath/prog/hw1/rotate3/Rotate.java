11f43342e98b7b2be7453a3b7027628fa16d796f
kseniiaguk <43299642+kseniiaguk@users.noreply.github.com>
2019-04-17 00:26:05 +0300

    update
    smth new

--- sem2/src/ru/spbu/apmath/prog/hw1/rotate3/Rotate.java
+++ sem2/src/ru/spbu/apmath/prog/hw1/rotate3/Rotate.java
@@ -3,12 +3,16 @@ package ru.spbu.apmath.prog.hw1.rotate3;
 import java.util.ArrayList;
 
 public class Rotate {
-    public ArrayList rotate(ArrayList input){
-        ArrayList answer = new ArrayList();
-        answer.add(0,input.get(input.size()-1));
-        for (int i = 1; i < input.size(); i++){
+    public ArrayList<Integer> rotate(ArrayList<Integer> input){
+        ArrayList<Integer> answer = new ArrayList<>();
+        if (input.size() != 0){
+            answer.add(0,input.get(input.size()-1));
+            for (int i = 1; i < input.size(); i++){
             answer.add(i, input.get(i-1));
+            }
+            return answer;}
+        else{
+            return answer;
         }
-        return answer;
     }
 }
